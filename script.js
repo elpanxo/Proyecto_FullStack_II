18987c94c1ee55b9171b8a606c434bf3fe0d8bb5
1tokkio <matiasvargas124@gmail.com>
2025-09-13 13:08:35 -0300

    first commit

--- script.js
+++
@@ -1,5 +0,0 @@
-function showContent(sectionId){
-    document.querySelectorAll("main section").forEach(sec => sec.classList.add("hidden"));
-
-    document.getElementById(sectionId).classList.remove("hidden");
-}
